61950c77b008cbbb667ae6f64887c2557c262302
Abhay Chothani <abhay.chothani@girirajdigital.com>
2026-06-25 09:51:21 +0530

    Hard , Soft Assertion

--- tests/Assertion.spec.js
+++ tests/Assertion.spec.js
@@ -4,21 +4,27 @@ import { test, expect } from '@playwright/test';
 test('AssertionTest', async ({ page }) => {
 
     //open app url
-    await page.goto('https://demo.nopcommerce.com/register')
+    await page.goto('https://www.girirajdigital.com/', { 
+        waitUntil: 'domcontentloaded'
+    })
 
     // 1) pages has URL
-    await expect(page).toHaveURL('https://demo.nopcommerce.com/register')
+    await expect(page).toHaveURL('https://www.girirajdigital.com/')
 
     //2) Page has title
-    await expect(page).toHaveTitle('nopCommerce demo store. Register')
+    await expect(page).toHaveTitle('Enterprise Software Development Company | GIRIRAJ DIGITAL')
 
-
-    await expect(page.locator('title')).toBeVisible();
-    await expect(page).toHaveTitle(/nopCommerce demo store. Register/);
-
-
-    ///3) element is visible
-    const logoelement = await page.locator('.header-logo')
+    //3) element is visible
+    const logoelement = await page.locator('.navbar-brand')
     await expect(logoelement).toBeVisible();
+
+    //4) Control is enable
+    const viewbutton = await page.locator('.navigation')
+    await expect(viewbutton).toBeEnabled()
+
+
+
+
+
 })
 
---
+++ tests/SoftAssertion.spec.js
@@ -0,0 +1,26 @@
+import { test, expect } from '@playwright/test';
+
+test ("Soft Assertion", async ({page})=>{
+
+// Hard Assertion :  Test execution stops immediately if assertion fails.	
+// Soft Assertion :  Test execution continues even if assertion fails.
+
+    //await page.goto("https://www.demoblaze.com/index.html")
+    await page.goto("https://www.demoblaze.com/index.html", {
+         waitUntil: "domcontentloaded" 
+        
+        });
+
+
+    // Hard Assertion
+    // await expect(page).toHaveTitle("STORE");
+    // await expect(page).toHaveURL("https://www.demoblaze.com/index.html");
+    // await expect(page.locator('.navbar-brand')).toBeVisible();
+
+    
+    //soft Assertion
+    await expect.soft(page).toHaveTitle("STORE123");
+    await expect.soft(page).toHaveURL("https://www.demoblaze.com/index.html");
+    await expect.soft(page.locator('.navbar-brand')).toBeVisible();
+
+})
