7afe04026ded9ed18ea43a22806e43155572f3c6
Abhay Chothani <abhay.chothani@girirajdigital.com>
2026-06-29 19:06:47 +0530

    add comment

--- tests/Alert.spec.js
+++ tests/Alert.spec.js
@@ -44,6 +44,7 @@ test.skip('Confirmation Dialog-alert with OK and Cancel', async ({ page }) => {
 
 
 test('Prompt Dialog', async ({ page }) => {
+    //test
 
 
     await page.goto("https://the-internet.herokuapp.com/javascript_alerts");
