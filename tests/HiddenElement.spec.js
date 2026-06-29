09e1ac7c8d6354e400ffb813f6022039086a116f
Abhay Chothani <abhay.chothani@girirajdigital.com>
2026-06-29 19:04:25 +0530

    Perform Hidden Dorpdown & different dialog boxes

---
+++ tests/HiddenElement.spec.js
@@ -0,0 +1,31 @@
+import { test, expect } from '@playwright/test';
+
+
+test('Hidden Option dropdown', async ({ page }) => {
+
+    await page.goto("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login")
+    await page.locator("[name='username']").fill('Admin');
+    await page.locator("[name='password']").fill('admin123');
+    await page.locator("[type='submit']").click();
+
+    await page.locator("//span[normalize-space()='PIM']").click()
+
+    // clicking on job title dropdown 
+    await page.locator("//body[1]/div[1]/div[1]/div[2]/div[2]/div[1]/div[1]/div[2]/form[1]/div[1]/div[1]/div[6]/div[1]/div[2]/div[1]/div[1]/div[2]/i[1]").click();
+
+    //waiting for option
+
+    await page.waitForTimeout(3000);
+    const options = await page.$$("//div[@role='listbox']//span")
+    
+    for(let option of options){
+        const jobTitle = await option.textContent();
+       // console.log(jobTitle);      
+       if(jobTitle.includes("QA Engineer")){
+        await option.click();
+        break;
+       }
+    }
+    await page.waitForTimeout(5000);
+
+});
