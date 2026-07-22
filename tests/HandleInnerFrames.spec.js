dd0f24059b27c5676ffafdf86263dc9bcdf8e894
Abhay Chothani <abhay.chothani@girirajdigital.com>
2026-07-22 11:36:34 +0530

    changes
    due to workflow fail in particular browser

--- tests/HandleInnerFrames.spec.js
+++ tests/HandleInnerFrames.spec.js
@@ -1,24 +1,35 @@
 import { test, expect } from '@playwright/test';
 
+// test('Inner frames', async ({ page }) => {
+//     await page.goto("https://ui.vision/demo/webtest/frames/");
+
+//     //total frame
+//     // const alframes = await page.frames();
+//     // console.log("Number of frames : ",alframes.length)
+//     await page.waitForLoadState();
+
+
+//     const frame3 = await page.frame({ url: 'https://ui.vision/demo/webtest/frames/frame_3' });
+//     //frmae3.locator("input[name='mytext3']").fill('welcome');
+
+//     //nested frame
+//     const childframe = await frame3.childFrames()
+//     await childframe[0].locator("//*[@id='i6']/div[3]/div").check()
+//     //await childframe[0].getByLabel('I am a human').check();
+
+// })
+//-------------------------------------------------------------------------------------------------------
 test('Inner frames', async ({ page }) => {
-    await page.goto("https://ui.vision/demo/webtest/frames/");
+  await page.goto('https://ui.vision/demo/webtest/frames/');
 
-    //total frame
-    // const alframes = await page.frames();
-    // console.log("Number of frames : ",alframes.length)
-    await page.waitForLoadState();
+  const googleForm = page
+    .frameLocator("frame[src='frame_3.html']")
+    .frameLocator("iframe");
 
+  await googleForm.locator('#i6').click();
+});
+//-------------------------------------------------------------------------------------------------------
 
-    const frame3 = await page.frame({ url: 'https://ui.vision/demo/webtest/frames/frame_3' });
-    //frmae3.locator("input[name='mytext3']").fill('welcome');
-
-    //nested frame
-    const childframe = await frame3.childFrames()
-    await childframe[0].locator("//*[@id='i6']/div[3]/div").check()
-    //await childframe[0].getByLabel('I am a human').check();
-
-    
-})
 
 
 
